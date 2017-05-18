a3e39107587a60c55f1d9d945f11196d8a7686ea
E Camden Fisher <fish@fishnix.net>
2017-05-18 09:43:28 -0400

    Update README, add example

---
+++ example/scripts/cleanup.sh
@@ -0,0 +1,11 @@
+echo "Cleaning up ..."
+
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi bitstream-vera-fonts
+yum -y clean all
+rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
+rm -rf /etc/yum.repos.d/mysql*.repo
+rm -rf VBoxGuestAdditions_*.iso
+
+# Remove traces of mac address from network configuration
+sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
+rm -f /etc/udev/rules.d/70-persistent-net.rules
---
+++ example/scripts/zerodisk.sh
@@ -0,0 +1,5 @@
+echo "Zeroing out free space ..."
+
+# Zero out the free space to save space in the final image:
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
