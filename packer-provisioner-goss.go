a86e22341e37f86aa3db0e4e0d22e172ac6f4b4c
E Camden Fisher <fish@fishnix.net>
2020-01-27 06:41:55 -0500

    Go modules and updates for Packer 1.5 API (#21)
    * Go modules and updates for packer 1.5 API
    * Add support for retries and timeouts

--- packer-provisioner-goss.go
+++ packer-provisioner-goss.go
@@ -1,11 +1,15 @@
+//go:generate mapstructure-to-hcl2 -type GossConfig
+
 package main
 
 import (
-    "context"
+	"context"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/hashicorp/hcl/v2/hcldec"
 
 	"github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/packer"
@@ -30,11 +34,15 @@ type GossConfig struct {
 	// An array of tests to run.
 	Tests []string
 
+	// Goss options for retry and timeouts
+	RetryTimeout string `mapstructure:"retry_timeout"`
+	Sleep        string `mapstructure:"sleep"`
+
 	// Use Sudo
 	UseSudo bool `mapstructure:"use_sudo"`
 
 	// skip ssl check flag
-	SkipSSLChk   bool `mapstructure:"skip_ssl"`
+	SkipSSLChk bool `mapstructure:"skip_ssl"`
 
 	// The --gossfile flag
 	GossFile string `mapstructure:"goss_file"`
@@ -77,6 +85,10 @@ func main() {
 	server.Serve()
 }
 
+func (p *Provisioner) ConfigSpec() hcldec.ObjectSpec {
+	return p.config.FlatMapstructure().HCL2Spec()
+}
+
 // Prepare gets the Goss Privisioner ready to run
 func (p *Provisioner) Prepare(raws ...interface{}) error {
 	err := config.Decode(&p.config, &config.DecodeOpts{
@@ -91,7 +103,7 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 	}
 
 	if p.config.Version == "" {
-		p.config.Version = "0.3.2"
+		p.config.Version = "0.3.9"
 	}
 
 	if p.config.Arch == "" {
@@ -160,7 +172,7 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 }
 
 // Provision runs the Goss Provisioner
-func (p *Provisioner) Provision(ctx context.Context, ui packer.Ui, comm packer.Communicator) error {
+func (p *Provisioner) Provision(ctx context.Context, ui packer.Ui, comm packer.Communicator, generatedData map[string]interface{}) error {
 	ui.Say("Provisioning with Goss")
 
 	if !p.config.SkipInstall {
@@ -213,7 +225,7 @@ func (p *Provisioner) Provision(ctx context.Context, ui packer.Ui, comm packer.C
 		}
 	}
 
-	ui.Say("Running goss tests...")
+	ui.Say("\n\n\nRunning goss tests...")
 	if err := p.runGoss(ui, comm); err != nil {
 		return fmt.Errorf("Error running Goss: %s", err)
 	}
@@ -224,7 +236,7 @@ func (p *Provisioner) Provision(ctx context.Context, ui packer.Ui, comm packer.C
 // installGoss downloads the Goss binary on the remote host
 func (p *Provisioner) installGoss(ui packer.Ui, comm packer.Communicator) error {
 	ui.Message(fmt.Sprintf("Installing Goss from, %s", p.config.URL))
-    ctx := context.TODO()
+	ctx := context.TODO()
 
 	cmd := &packer.RemoteCmd{
 		// Fallback on wget if curl failed for any reason (such as not being installed)
@@ -254,8 +266,9 @@ func (p *Provisioner) runGoss(ui packer.Ui, comm packer.Communicator) error {
 
 	cmd := &packer.RemoteCmd{
 		Command: fmt.Sprintf(
-			"cd %s && %s %s %s %s %s validate %s",
-			p.config.RemotePath, p.enableSudo(), goss, p.config.GossFile, p.vars(), p.debug(), p.format()),
+			"cd %s && %s %s %s %s %s validate --retry-timeout %s --sleep %s %s",
+			p.config.RemotePath, p.enableSudo(), goss, p.config.GossFile,
+			p.vars(), p.debug(), p.retryTimeout(), p.sleep(), p.format()),
 	}
 	if err := cmd.RunWithUi(ctx, comm, ui); err != nil {
 		return err
@@ -265,6 +278,20 @@ func (p *Provisioner) runGoss(ui packer.Ui, comm packer.Communicator) error {
 	}
 	ui.Say(fmt.Sprintf("Goss tests ran successfully"))
 	return nil
+}
+
+func (p *Provisioner) retryTimeout() string {
+	if p.config.RetryTimeout == "" {
+		return "0s" // goss default
+	}
+	return p.config.RetryTimeout
+}
+
+func (p *Provisioner) sleep() string {
+	if p.config.Sleep == "" {
+		return "1s" // goss default
+	}
+	return p.config.Sleep
 }
 
 // debug returns the debug flag if debug is configured
@@ -291,7 +318,7 @@ func (p *Provisioner) vars() string {
 
 func (p *Provisioner) sslFlag(cmdType string) string {
 	if p.config.SkipSSLChk {
-		switch(cmdType) {
+		switch cmdType {
 		case "curl":
 			return "-k"
 		case "wget":
@@ -314,7 +341,7 @@ func (p *Provisioner) enableSudo() string {
 // Deal with curl & wget username and password
 func (p *Provisioner) userPass(cmdType string) string {
 	if p.config.Username != "" {
-		switch(cmdType) {
+		switch cmdType {
 		case "curl":
 			if p.config.Password == "" {
 				return fmt.Sprintf("-u %s", p.config.Username)
@@ -326,7 +353,7 @@ func (p *Provisioner) userPass(cmdType string) string {
 			}
 			return fmt.Sprintf("--user=%s --password=%s", p.config.Username, p.config.Password)
 		default:
-			return  ""
+			return ""
 		}
 	}
 	return ""
